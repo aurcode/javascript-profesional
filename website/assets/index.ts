1141869b7fb79e1f928aeb52c5c6370afcd701b8
aurcode <aurelio3927@gmail.com>
2020-12-04 09:46:35 -0400

    npm public and website

--- website/assets/index.ts
+++ website/assets/index.ts
@@ -1,7 +1,7 @@
-import MediaPlayer from './MediaPlayer';
-import AutoPlay from './plugins/AutoPlay';
-import AutoPause from './plugins/AutoPause';
-import Ads from './plugins/Ads';
+import MediaPlayer from '@aurcode/platzimediaplayer';
+import AutoPlay from '@aurcode/platzimediaplayer/lib/plugins/AutoPlay';
+import AutoPause from '@aurcode/platzimediaplayer/lib/plugins/AutoPause';
+import Ads from '@aurcode/platzimediaplayer/lib/plugins/Ads';
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({ el: video, plugins: [new AutoPlay(), new AutoPause(), new Ads], });
